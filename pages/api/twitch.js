cc517664f4f33f3ea4065ee9734d54d42b7afa05
wakfuld <63659885+wakfuld23@users.noreply.github.com>
2021-08-24 20:48:55 +0000

    Add: getTwitchChannel and filter out the stremear searched

--- pages/api/twitch.js
+++ pages/api/twitch.js
@@ -1,11 +1,17 @@
 // This is where all the logic for your Twitch API will live!
 export default async (req, res) => {
   try {
-    if(req.method === 'POST'){
-      const {data} = req.body
-      res.status(200).json({data})
+    if (req.method === 'POST') {
+      const { data } = req.body
+      const channelData = await getTwitchChannel(data)
+      if (channelData) {
+        console.log("CHANNEL DATA: ", channelData)
+        res.status(200).json({ channelData })
+
+      }
+      res.status(404).send()
     }
-  } catch(error){
+  } catch (error) {
     console.warn(error.message)
     res.status(500).send()
   }
@@ -15,7 +21,7 @@ export default async (req, res) => {
 const getTwitchAccesToken = async () => {
   console.log('GETTING ACCES TOKEN...')
 
-  const path = `https://id.twitch.tv/oauth2/token?client_id=${porcess.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_SECRET_ID}&grant_type=client_credentials`
+  const path = `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_SECRET_ID}&grant_type=client_credentials`
 
   const response = await fetch(path, {
     method: 'POST'
@@ -27,3 +33,38 @@ const getTwitchAccesToken = async () => {
   }
 }
 
+const getTwitchChannel = async  channelName => {
+  console.log('SEARCHING FOR TWITCH CHANNEL')
+
+  if(channelName){
+    //Get acces token
+    const accesToken = await getTwitchAccesToken()
+
+    if(accesToken) {
+      //Make query request
+      const response = await fetch(`https://api.twitch.tv/helix/search/channels?query=${channelName}`, {
+        headers: {
+          Authorization: `Bearer ${accesToken}`,
+          "Client-Id": process.env.TWITCH_CLIENT_ID
+        }
+      })
+
+      const json = await response.json()
+      if(json.data){
+        const {data} = json
+
+        
+        const lowercaseChannelName = channelName.toLowerCase()
+
+        const foundChannel = data.find(channel => {
+          const lowercaseDisplayName = channel.display_name.toLowerCase()
+          return lowercaseChannelName === lowercaseDisplayName
+        })
+        return foundChannel
+      }
+    }
+    
+    throw new Error("Twitch accesToken was undefined")
+  }
+  throw new Error('No channelName provided')
+}
