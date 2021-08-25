0f0960b3246ee78b39604f7fcfca58fbef9f7ea8
wakfuld <63659885+wakfuld23@users.noreply.github.com>
2021-08-25 12:28:42 +0000

    Add: updating values in Replit DB

--- components/StreamerGrid.js
+++ components/StreamerGrid.js
@@ -2,8 +2,33 @@ import Image from 'next/image'
 import styles from '../styles/StreamerGrid.module.css'
 const StreamerGrid = ({ channels, setChannels }) => {
   //Actions
-  const removeChannelAction = channelId => () => {
-    setChannels(channels.filter(channel => channel.id !== channelId))
+  const removeChannelAction = channelId => async () => {
+    const filteredChannels = channels.filter(channel => channel.id !== channelId)
+
+    setChannels(filteredChannels)
+
+    const joinedChannels = filteredChannels.map(channel => channel.display_name.toLowerCase()).join(",")
+
+    await setDBChannels(joinedChannels)
+  }
+
+  const setDBChannels = async channels => {
+    try {
+      const path = `https://${window.location.hostname}`
+
+      const response = await fetch(`${path}/api/database`, {
+        method: 'POST',
+        body: JSON.stringify({
+          key: 'CHANNELS',
+          value: channels,
+        })
+      })
+      if (response.status === 200){
+        console.log(`Set ${channles} in DB`)
+      } 
+    } catch (error){
+        console.log(error.message)
+      }
   }
 
   //RenderMethod
